899452961c5004b4fa59547a0a84178246ddd417
zaidalyafeai <alyafey22@gmail.com>
2021-07-11 16:18:02 +0300

    test using multi modelling

--- nmatheg/nmatheg.py
+++ nmatheg/nmatheg.py
@@ -9,21 +9,25 @@ import configparser
 class TrainStrategy:
   def __init__(self, config_path):
 
-    config = configparser.ConfigParser()
-    config.read(config_path)
+    self.config = configparser.ConfigParser()
+    self.config.read(config_path)
 
-    data_config = configparser.ConfigParser()
+    self.data_config = configparser.ConfigParser()
     rel_path = os.path.dirname(__file__)
     data_ini_path = os.path.join(rel_path, "datasets.ini")
-    data_config.read(data_ini_path)
-
-    self.train_config, self.model_config = create_configs(config, data_config)
-    self.datasets = create_dataset(config, data_config)
-
-    if 'bert' in self.model_config['model_name']:
-      self.model = BERTClassificationModel(self.model_config)
-    else:
-      self.model = SimpleClassificationModel(self.model_config)
+    self.data_config.read(data_ini_path)
 
   def start(self):
-    self.model.train(self.datasets, **self.train_config)
+    
+    self.train_config, self.model_config = create_configs(self.config, self.data_config)
+    self.datasets = create_dataset(self.config, self.data_config)
+    model_names = self.model_config['model_name'].split(',')
+
+    for model_name in model_names:
+      self.model_config['model_name'] = model_name
+      if 'bert' in model_name:
+        self.model = BERTClassificationModel(self.model_config)
+      else:
+        self.model = SimpleClassificationModel(self.model_config)
+
+      self.model.train(self.datasets, **self.train_config)
