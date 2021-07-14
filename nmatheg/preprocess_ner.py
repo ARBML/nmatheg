e638a91a1b11e6200abdc74b0baa539bc17bd668
zaidalyafeai <alyafey22@gmail.com>
2021-07-14 21:48:53 +0300

    fix bugs and cleanup

--- nmatheg/preprocess_ner.py
+++ nmatheg/preprocess_ner.py
@@ -5,8 +5,8 @@ from datasets import Dataset, DatasetDict
 def aggregate_tokens(dataset, config, data_config, max_len = 128):
     new_dataset = {}
     dataset_name = config['dataset']['dataset_name']
-    token = data_config[dataset_name]['text']
-    tag = data_config[dataset_name]['label']
+    token_col = data_config[dataset_name]['text']
+    tag_col = data_config[dataset_name]['label']
 
     for split in dataset:
         sent_labels = []
@@ -15,7 +15,7 @@ def aggregate_tokens(dataset, config, data_config, max_len = 128):
         sentences = []
         
         for i, item in enumerate(dataset[split]):
-            token, label = item[token], item[tag]
+            token, label = item[token_col], item[tag_col]
             sent_label.append(label)
             sentence.append(token)
             if len(sentence) == max_len:
@@ -23,18 +23,18 @@ def aggregate_tokens(dataset, config, data_config, max_len = 128):
                 sent_labels.append(sent_label)
                 sentence = []
                 sent_label = []
-        new_dataset[split] = Dataset.from_dict({token:sentences, tag:sent_labels}) 
+        new_dataset[split] = Dataset.from_dict({token_col:sentences, tag_col:sent_labels}) 
     return DatasetDict(new_dataset) 
 
 # https://github.com/huggingface/transformers/blob/44f5b260fe7a69cbd82be91b58c62a2879d530fa/examples/pytorch/token-classification/run_ner_no_trainer.py#L353
 def tokenize_and_align_labels(dataset, tokenizer, config, data_config, max_len = 128):
 
     dataset_name = config['dataset']['dataset_name']
-    token = data_config[dataset_name]['text']
-    tag = data_config[dataset_name]['label']
+    token_col = data_config[dataset_name]['text']
+    tag_col = data_config[dataset_name]['label']
 
     tokenized_inputs = tokenizer(
-        dataset[token],
+        dataset[token_col],
         max_length=max_len,
         padding='max_length',
         truncation=True,
@@ -42,7 +42,7 @@ def tokenize_and_align_labels(dataset, tokenizer, config, data_config, max_len =
         is_split_into_words=True,
     )
     labels = []
-    for i, label in enumerate(dataset[tag]):
+    for i, label in enumerate(dataset[tag_col]):
         word_ids = tokenized_inputs.word_ids(batch_index=i)
         previous_word_idx = None
         label_ids = []
