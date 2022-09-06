f9a9d26c1caf6a5c39a7088cda8d5e071b44a3ce
zaidalyafeai <alyafey22@gmail.com>
2022-09-06 19:47:37 +0300

    remove special case for tatoeba_mt

--- nmatheg/dataset.py
+++ nmatheg/dataset.py
@@ -80,16 +80,9 @@ def create_dataset(config, data_config, vocab_size = 300,
     # clean and load data
     # load_dataset_kwargs = config['load_dataset_kwargs']
     # dataset = load_dataset(dataset_name,**load_dataset_kwargs)
-    try:
-        if dataset_name == "tatoeba_mt":
-            print(f"downloading {dataset_name} manually ... ")
-            clone = "git lfs clone https://huggingface.co/datasets/Helsinki-NLP/tatoeba_mt"
-            wget = "wget https://huggingface.co/datasets/Zaid/tatoeba_mt/raw/main/tatoeba_mt.py -O tatoeba_mt/tatoeba_mt.py"
-
-            os.system(f"{clone} && {wget}")
-
+    if 'subset' in data_config:
         dataset = load_dataset(dataset_name, data_config['subset'])
-    except:
+    else:
         dataset = load_dataset(dataset_name)
     
     
